ed0474e1503f887c673b34de54a236ba262b86b7
PierreVieira <pierrevieiraggg@gmail.com>
2025-09-21 12:35:34 -0300

    Add placeholder Koin module for `episodes` presentation layer

---
+++ feature/episodes/presentation/src/commonMain/kotlin/org/pierre/tvmaze/feature/episodes/presentation/di/EpisodesPresentationModule.kt
@@ -0,0 +1,9 @@
+package org.pierre.tvmaze.feature.episodes.presentation.di
+
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+// Placeholder Koin module for Episodes presentation layer
+val episodesPresentationModule: Module = module {
+    // define presentation dependencies here (viewModels, presenters, etc.)
+}
