844320f44b27a2b80d7edbda17481b923552b8b7
PierreVieira <pierrevieiraggg@gmail.com>
2025-09-21 01:12:40 -0300

    Remove unused `FavoritesState` data model from `favorites` presentation layer

--- feature/favorites/presentation/src/commonMain/kotlin/org/pierre/tvmaze/feature/favorites/presentation/model/FavoritesState.kt
+++
@@ -1,7 +0,0 @@
-package org.pierre.tvmaze.feature.favorites.presentation.model
-
-import org.pierre.tvmaze.model.common.ShowItemModel
-
-data class FavoritesState(
-    val items: List<ShowItemModel> = emptyList()
-)
